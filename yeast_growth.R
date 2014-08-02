55a45f1d3a8f7b1dab3f0520f3bc45cb44e0cfe2
Geno V <g.villafano@gmail.com>
2014-08-02 20:54:49 +0200

    added example of how to manually do it, but it sucks

--- yeast_growth.R
+++ yeast_growth.R
@@ -3,6 +3,7 @@
 
 
 library(reshape)
+library(ggplot2)
 
 growth <- file.choose()
 
@@ -43,24 +44,49 @@ df.avg
 
 df.deviations <- as.data.frame(deviations)
 colnames(df.deviations) <- x
+df.deviation
 df.deviations$Time <- df$time.1.8.
 
 #some additional setup for the plot
-df.avg <- melt(df.avg ,  id = 'Time', variable_name = 'Sample')
+df.avg.melt <- melt(df.avg ,  id = 'Time', variable_name = 'Sample')
 df.dvs <- melt(df.deviations, id = 'Time', variable_name = 'Sample') 
-df.avg$sd <- df.dvs[3]
-colnames(df.avg) <- c('Time', 'Sample', 'OD', 'sd')
-df.avg
+df.avg.melt$sd <- df.dvs[3]
+colnames(df.avg.melt) <- c('Time', 'Sample', 'OD', 'sd')
+#dataframe of all data with the standard error
+df.avg.melt
 
-error <- aes( ymin= OD - sd, ymax= OD + sd)
-
-growth.plot <- ggplot(df.avg, aes(Time,OD), stat = 'identity') +
+#how I would normally add the standard error to a plot
+error <- aes(ymin = OD - sd, ymax = OD + sd)
+#final plot
+growth.plot <- ggplot(df.avg.melt, aes(Time,OD), stat = 'identity') +
   geom_line(aes(col = Sample)) +
     geom_point(aes(col = Sample)) +
-      geom_errorbar(error, col = 'tan', width = .2, size = 0.8, position = position_dodge(.9)) + 
+      #doesn't work with this active idk why
+      #geom_errorbar(error, col = 'tan', width = .2, size = 0.8, position = position_dodge(.9)) + 
         labs(x = 'Time (min)', y = 'OD' )
 
 print(growth.plot)
+
+
+dvs.1 <- df.deviations$ymk118.18
+df.1 <- list(y = df.avg$ymk118.18, x = df.avg$Time, sd = dvs.1)
+df.1 <- as.data.frame(df.1)
+
+##AN EXMAPLE WITH ERROR BARS (individually would have to add each plot layer by layer)
+plot1 <- ggplot(df.1, aes(x=x,y=y), stat = 'identity') +
+  geom_errorbar(error1, size = 0.8, width = 20, col = 'grey')+        
+  geom_line() + geom_point()
+
+
+print(plot1)
+
+error1 <- aes(ymin = df.avg[[1]] - df.deviations[[1]], ymax = df.avg[[1]]+ df.deviations[[1]])
+error2 <- aes(ymin = df.avg[[2]] - df.deviations[[2]], ymax = df.avg[[2]]+ df.deviations[[2]])
+error3 <- aes(ymin = df.avg[[3]] - df.deviations[[3]], ymax = df.avg[[3]]+ df.deviations[[3]])
+error4 <- aes(ymin = df.avg[[4]] - df.deviations[[4]], ymax = df.avg[[4]]+ df.deviations[[4]])
+
+
+
 
 test <- plotly(username="g.villafano", key="nbny7yk7g3")
 
